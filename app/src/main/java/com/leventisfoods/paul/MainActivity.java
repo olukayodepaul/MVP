a54960fa37a164d84a2a4820833f9d3070fc132b
paul <paul.oluakyode.pro@gmail.com>
2018-12-18 20:48:41 +0100

    commit

--- app/src/main/java/com/leventisfoods/paul/MainActivity.java
+++ app/src/main/java/com/leventisfoods/paul/MainActivity.java
@@ -13,9 +13,7 @@ public class MainActivity extends AppCompatActivity implements MainContract.MvpV
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         mainPresenter = new MainPresenter(this);
-
 
     }
 
