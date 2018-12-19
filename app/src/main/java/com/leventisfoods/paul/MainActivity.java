50f9b1e3f56623f012418f80ff14450acd8fa2d6
paul <paul.oluakyode.pro@gmail.com>
2018-12-19 05:30:40 +0100

    commit main changes

--- app/src/main/java/com/leventisfoods/paul/MainActivity.java
+++ app/src/main/java/com/leventisfoods/paul/MainActivity.java
@@ -10,6 +10,7 @@ public class MainActivity extends AppCompatActivity implements MainContract.MvpV
     private MainPresenter mainPresenter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mainPresenter = new MainPresenter(this);
